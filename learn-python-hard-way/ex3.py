b71d300f0653cf63a5700b69fc31fb0e2c946a79
Matheus Dias <matheusdyas007@hotmail.com>
2022-11-10 11:45:54 -0300

    gitignore and LPTHW first three exercises

---
+++ learn-python-hard-way/ex3.py
@@ -0,0 +1,41 @@
+#print a phrase
+print("I will now count my chickens:")
+
+#through addition and division print the number of hens
+print("Hens", 25.0 + 30.0 / 6.0)
+
+#through substraction, multiplication and mod print the number of rooster
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+#print a phrase
+print("Now I will count the eggs:")
+
+#through addition, substraction, mod and division print the number of eggs
+print( 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+#print a phrase
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#print the boolean result of an expression
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+#print an addition
+print("What is 3 + 2?", 3.0 + 2.0)
+
+#print a substraction
+print("What is 5 - 7?", 5.0 - 7.0)
+
+#print a phrase
+print("Oh, that's why it's False.")
+
+#print a phrase
+print("How about some more.")
+
+#print the boolean value of an expression
+print("Is it greater?", 5.0 > -2.0)
+
+#print the boolean value of an expression
+print("Is it greater or equal?", 5.0 >= -2.0)
+
+#print the boolean value of an expression
+print("Is it less or equal?", 5.0 <= -2.0)
